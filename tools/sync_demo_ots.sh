21d56108cc47419ba67b80e12c872acc1a24a186
Mu2E <mu2edaq@mu2edaq01.fnal.gov>
2019-01-11 17:11:05 -0600

    added sync demo script to be tracked in repo

--- tools/sync_demo_ots.sh
+++ tools/sync_demo_ots.sh
@@ -8,10 +8,11 @@ source /home/mu2edaq/sync_demo/ots/setup_ots.sh
 Reset=`tput sgr0`         # Reset all  
 
 echo -e "${Reset}===> start otsdaq on $HOSTNAME${Reset}"
+
 StartOTS.sh
 
 if [[ "$HOSTNAME" == "mu2edaq06.fnal.gov" ]]; then
-    echo "Starting OTS on remote machines (XDAQ supervisors only)"
+    echo -e "${Reset}Starting OTS on remote machines (XDAQ supervisors only)${Reset}"
     ssh mu2edaq04 /home/mu2edaq/sync_demo/ots/sync_demo_ots.sh &
     ssh mu2edaq05 /home/mu2edaq/sync_demo/ots/sync_demo_ots.sh &
 #    ssh mu2edaq02 /home/mu2edaq/sync_demo/ots/sync_demo_ots.sh
