9e82aa80d83e77dc272f2e072e283905f34c194b
Mu2E <mu2edaq@mu2edaq01.fnal.gov>
2019-01-11 17:05:33 -0600

    added sync demo script to be tracked in repo

---
+++ tools/sync_demo_ots.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "setup otsdaq on $HOSTNAME"
+
+#export KRB5CCNAME=FILE:/tmp/krb5cc_otsdaq_develop_daq
+source /home/mu2edaq/sync_demo/ots/setup_ots.sh
+
+Reset=`tput sgr0`         # Reset all  
+
+echo -e "${Reset}===> start otsdaq on $HOSTNAME${Reset}"
+StartOTS.sh
+
+if [[ "$HOSTNAME" == "mu2edaq06.fnal.gov" ]]; then
+    echo "Starting OTS on remote machines (XDAQ supervisors only)"
+    ssh mu2edaq04 /home/mu2edaq/sync_demo/ots/sync_demo_ots.sh &
+    ssh mu2edaq05 /home/mu2edaq/sync_demo/ots/sync_demo_ots.sh &
+#    ssh mu2edaq02 /home/mu2edaq/sync_demo/ots/sync_demo_ots.sh
+fi
