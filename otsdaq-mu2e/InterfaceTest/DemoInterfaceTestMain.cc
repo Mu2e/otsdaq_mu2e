d6af32ae1ad13c34400846237a57b85ed763a528
Ryan Rivera <rrivera@fnal.gov>
2019-02-21 07:29:43 -0600

    re-clangified

--- otsdaq-mu2e/InterfaceTest/DemoInterfaceTestMain.cc
+++ otsdaq-mu2e/InterfaceTest/DemoInterfaceTestMain.cc
@@ -1,10 +1,10 @@
 #include <fstream>
 #include <iostream>
 
-#include "otsdaq-core/TableCore/TableGroupKey.h"
 #include "otsdaq-components/FEInterfaces/FEOtsUDPTemplateInterface.h"
 #include "otsdaq-core/ConfigurationInterface/ConfigurationInterface.h"
 #include "otsdaq-core/ConfigurationInterface/ConfigurationManager.h"
+#include "otsdaq-core/TableCore/TableGroupKey.h"
 /*
 
 #include "otsdaq-components/FEInterfaces/FEWOtsUDPFSSRInterface.h"
@@ -35,8 +35,7 @@ int main(int argc, char** argv)
 	std::pair<std::string, TableGroupKey> theGroup(configurationGroupName,
 	                                               TableGroupKey(configurationKeyValue_));
 
-	theConfigurationManager_->loadTableGroup(
-	    theGroup.first, theGroup.second, true);
+	theConfigurationManager_->loadTableGroup(theGroup.first, theGroup.second, true);
 
 	FEOtsUDPTemplateInterface* theInterface_ = new FEOtsUDPTemplateInterface(
 	    interfaceUID_,
