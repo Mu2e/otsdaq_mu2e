74e063cf9323c0ea0a24c59931e1bd3b65d66a09
Mu2E <mu2edaq@mu2edaq01.fnal.gov>
2019-03-26 09:33:59 -0500

    modify ROC read block to the correct DTC low-level software method

--- otsdaq-mu2e/FEInterfaces/DTCFrontEndInterface_interface.cc
+++ otsdaq-mu2e/FEInterfaces/DTCFrontEndInterface_interface.cc
@@ -158,7 +158,7 @@ void DTCFrontEndInterface::registerFEMacros(void)
 	registerFEMacroFunction("ROC_ReadBlock",
 			static_cast<FEVInterface::frontEndMacroFunction_t>(
 					&DTCFrontEndInterface::ReadROCBlock),
-					std::vector<std::string>{"rocLinkIndex", "block", "address"},
+					std::vector<std::string>{"rocLinkIndex", "numberOfWords", "address"},
 					std::vector<std::string>{"readData"},
 					1);  // requiredUserPermissions
 
@@ -2753,26 +2753,33 @@ void DTCFrontEndInterface::ReadROCBlock(__ARGS__)
 	DTCLib::DTC_Link_ID rocLinkIndex =
 	    DTCLib::DTC_Link_ID(__GET_ARG_IN__("rocLinkIndex", uint8_t));
 	uint8_t address = __GET_ARG_IN__("address", uint8_t);
-	uint8_t block   = __GET_ARG_IN__("block", uint8_t);
+	uint8_t number_of_words   = __GET_ARG_IN__("numberOfWords", uint8_t);
 
 	__FE_COUTV__(rocLinkIndex);
-	__FE_COUT__ << "block = " << std::dec << (unsigned int)block << __E__;
-	__FE_COUT__ << "address = 0x" << std::hex << (unsigned int)address << std::dec
-	            << __E__;
+	__FE_COUT__ << "address = 0x" << std::hex << (unsigned int)address << std::dec << __E__;
+	__FE_COUT__ << "numberOfWords = " << std::dec << (unsigned int)number_of_words << __E__;
 
-	uint16_t readData = thisDTC_->ReadExtROCRegister(rocLinkIndex, block, address);
+	//	uint16_t readData = thisDTC_->ReadExtROCRegister(rocLinkIndex, block, address);
+	
+	auto readData = thisDTC_->ReadROCBlock(rocLinkIndex, address, number_of_words);
 
-	__SET_ARG_OUT__("readData", readData);
+	for (int i=0; i<number_of_words; i++) {
+		__FE_COUT__ << "read data [" << std::dec << i << "]  = 0x" << std::hex << readData[i] << __E__;
+	}
 
-	for(auto& argOut : argsOut)
-		__FE_COUT__ << argOut.first << ": " << argOut.second << __E__;
+	//__SET_ARG_OUT__("readData", readData);
+
+	//for(auto& argOut : argsOut)
+	//  __FE_COUT__ << argOut.first << ": " << argOut.second << __E__;
 }
 
 //========================================================================
 void DTCFrontEndInterface::DTCHighRateDCSCheck(__ARGS__)
 {
-	for(auto& roc : rocs_)
+	for(auto& roc : rocs_) {
+		roc.second->writeRegister(21,10);  //hard-coded expectation for this value
 		roc.second->highRateCheck();
+	}
 }
 
 //========================================================================
