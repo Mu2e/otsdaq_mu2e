6fc14eaa60e8bd2785add41792c05e28028c28b3
Mu2E <mu2ehwdev@mu2edaq01.fnal.gov>
2019-04-04 12:06:11 -0500

    added generalized distributed otsdaq script

---
+++ tools/distributed_setup_ots.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+userinput=$1
+# the next "unsets" the command line input, so as not to pass it along unwittinging somewhere else
+shift        
+
+if [ x$userinput == "x" ]; then
+  echo "================================================="
+  echo "usage:  distributed_setup_ots.sh <subsystem>"
+  echo "... where  subsystem = (hwdev,sync)"
+  echo "================================================="
+  exit;
+fi
+
+basepath="notGoodBasepath"
+
+if [ $userinput == "sync" ]; then
+    basepath="mu2edaq/sync_demo"
+elif [ $userinput == "hwdev" ]; then
+    basepath="mu2ehwdev/test_stand"
+fi
+
+
+echo "setup otsdaq on $HOSTNAME"
+
+#export KRB5CCNAME=FILE:/tmp/krb5cc_otsdaq_develop_daq
+source /home/${basepath}/ots/setup_ots.sh ${userinput}
+
+Reset=`tput sgr0`         # Reset all  
+
+echo -e "${Reset}===> start otsdaq on $HOSTNAME${Reset}"
+
+ots #start ots in normal mode
+
+if [[ "$HOSTNAME" == "mu2edaq06.fnal.gov" ]]; then
+    echo -e "${Reset}Starting OTS on remote machines (XDAQ supervisors only)${Reset}"
+    ssh mu2edaq04 /home/${basepath}/ots/distributed_setup_ots.sh ${userinput} &
+    ssh mu2edaq05 /home/${basepath}/ots/distributed_setup_ots.sh ${userinput} &
+fi
