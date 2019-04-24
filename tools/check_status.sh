9616411311a6602b05580fcc343f1bed6a4084ab
Mu2E <mu2edaq@mu2edaq01.fnal.gov>
2019-04-24 11:36:14 -0500

    script to check git status

---
+++ tools/check_status.sh
@@ -0,0 +1,89 @@
+
+starting_dir=`pwd`
+echo "----------------------------------"
+cd pcie_linux_kernel_module
+echo `pwd`
+git fetch
+git status
+git remote -v
+cd $starting_dir
+
+echo "----------------------------------"
+cd otsdaq_components
+echo `pwd`
+git fetch
+git status
+git remote -v
+cd $starting_dir
+
+echo "----------------------------------"
+cd otsdaq
+echo `pwd`
+git fetch
+git status
+git remote -v
+cd $starting_dir
+
+echo "----------------------------------"
+cd otsdaq_utilities
+echo `pwd`
+git fetch
+git status
+git remote -v
+cd $starting_dir
+
+echo "----------------------------------"
+cd otsdaq_mu2e_calorimeter
+echo `pwd`
+git fetch
+git status
+git remote -v
+cd $starting_dir
+
+echo "----------------------------------"
+cd otsdaq_mu2e_trigger
+echo `pwd`
+git fetch
+git status
+git remote -v
+cd $starting_dir
+
+echo "----------------------------------"
+cd otsdaq_mu2e
+echo `pwd`
+git fetch
+git status
+git remote -v
+cd $starting_dir
+
+echo "----------------------------------"
+cd otsdaq_mu2e_crv
+echo `pwd`
+git fetch
+git status
+git remote -v
+cd $starting_dir
+
+echo "----------------------------------"
+cd otsdaq_mu2e_extmon
+echo `pwd`
+git fetch
+git status
+git remote -v
+cd $starting_dir
+
+echo "----------------------------------"
+cd otsdaq_mu2e_stm
+echo `pwd`
+git fetch
+git status
+git remote -v
+cd $starting_dir
+
+echo "----------------------------------"
+cd otsdaq_mu2e_tracker
+echo `pwd`
+git fetch
+git status
+git remote -v
+cd $starting_dir
