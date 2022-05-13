88f227a25a379c6b4015acb4294ad78cde165017
mu2ehwdev <rrivera@FNAL.GOV>
2022-05-13 09:51:32 -0500

    Allow for server naming convention in DAQ room

--- tools/vless_mu2e.sh
+++ tools/vless_mu2e.sh
@@ -2,9 +2,9 @@
 
 # Opens a remote file somewhat transparently using less
 
-echo "Opening remove file in less from mu2edaq${1}.fnal.gov: $2"
+echo "Opening remove file in less from mu2e${1}.fnal.gov: $2"
 
-scp mu2edaq${1}.fnal.gov:$2 .tmpLogFile && less .tmpLogFile && rm .tmpLogFile
+scp mu2e${1}.fnal.gov:$2 .tmpLogFile && less .tmpLogFile && rm .tmpLogFile
 
 
 
