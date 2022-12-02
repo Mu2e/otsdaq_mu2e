84ce3758b81676e336207d839fa29835eb8f2c18
Mu2E <mu2ehwdev@mu2e-dcs-01.fnal.gov>
2022-12-02 14:16:55 -0600

    Checking in local changes

--- otsdaq-mu2e/FEInterfaces/DTCFrontEndInterface.h
+++ otsdaq-mu2e/FEInterfaces/DTCFrontEndInterface.h
@@ -113,10 +113,7 @@ class DTCFrontEndInterface : public CFOandDTCCoreVInterface
 	void								ResetLossOfLockCounter			(__ARGS__);
 	void								ReadLossOfLockCounter			(__ARGS__);
 	void								GetUpstreamControlLinkStatus	(__ARGS__);
-	void								ResetLinkRx						(__ARGS__);
 	void								SelectJitterAttenuatorSource	(__ARGS__);
-	void								ShutdownLinkTx					(__ARGS__);
-	void								StartupLinkTx					(__ARGS__);
 	void								WriteDTC						(__ARGS__);
 	void								ReadDTC							(__ARGS__);
 	// clang-format on
--- tools/vtail_mu2e.sh
+++ tools/vtail_mu2e.sh
@@ -2,8 +2,19 @@
 
 # Opens a remote file somewhat transparently using tail -f
 
-echo "Opening remove file in less from mu2edaq${1}.fnal.gov: $2"
+if [ "x$1" == "x" ]; then
+    echo
+    echo
+    echo "    Usage: vtail_mu2e.sh <node name> <file name>"
+    echo
+    echo "    e.g.: vtail_mu2e.sh mu2e-cfo-01.fnal.gov /home/mu2ehwdev/ots/srcs/otsdaq_mu2e_config/Data_HWDev/Logs/otsdaq_quiet_run-gateway-mu2e-cfo-01.fnal.gov-3055.txt"
+    echo
+    echo
+    exit
+fi 
 
-ssh mu2edaq${1}.fnal.gov tail -f $2
+echo "Viewing file with 'tail -f' from node ${1}: $2"
+
+ssh ${1} tail -f $2
 
 
