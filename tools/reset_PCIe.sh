0192a49cf2ea8738de10aabef1c1638ffc10b5c8
RYAN A RIVERA <rrivera@FNAL.GOV>
2024-03-14 09:41:06 -0500

    resetPCIe.sh script improvements from Ron; also DTC FE plugin CFO clock marker interface fix.

--- tools/reset_PCIe.sh
+++ tools/reset_PCIe.sh
@@ -1,4 +1,5 @@
-source /home/xilinx/Vivado_Lab/2021.2/settings64.sh
+#!/bin/sh
+#source /home/xilinx/Vivado_Lab/2021.2/settings64.sh
 
 
 SCRIPT_DIR="$( 
@@ -7,9 +8,22 @@ SCRIPT_DIR="$(
 )"
 HOSTNAME="$(hostname -f)"
 
+echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 
+lspci | grep 'Xilinx.*7042' && foundXi=1 || foundXi=0
 
-lspci | grep Xilinx
+if [ "$foundXi" = 1 ];then
+    echo "Found DTC or CFO (Xilinx) card; removing mu2e driver;"
+    echo "first killing any processes that may be using the device."
+    pids=`lsof /dev/mu2e* 2>/dev/null | awk '!/^COMMAND/{print$2;}' | uniq`
+    test -n "$pids" && { echo "First attempt kill $pids"; kill $pids; }
+    killall xdaq.exe
+    sleep 3
+    rmmod mu2e
+else
+    echo 'No Xilinx 7042 cards found -- exiting'
+    exit 1
+fi
 
 echo
 echo "Resetting each PCIe Xilinx device on ${HOSTNAME}..."
@@ -26,14 +40,21 @@ do
     
     echo "1" > /sys/bus/pci/devices/0000:${array[0]}/remove
 
-done < <(lspci | grep Xilinx)
+done <<EOF
+$(lspci | grep 'Xilinx.*7042')
+EOF
 
 sleep 1
 echo "1" > /sys/bus/pci/rescan
+
+
+echo "Now attempt to reload mu2e module via modprobe mu2e"
+modprobe mu2e
+
 cd /root
 
 echo "Attempting to read firmware version on ${HOSTNAME}..."
-source setup_pcie.sh
+source ./setup_pcie.sh
 echo
 echo "PCIe Device 0 firmware version on ${HOSTNAME}:"
 my_cntl -d 0 read 0x9004 #device 0
