44a22d20d3497498311e82d4a823f699c216718a
Simon Corrodi <scorrodi@FNAL.GOV>
2024-04-16 18:39:43 -0500

    checking in latest state of SendAlarmToSlack.py and Wideband.html

--- UserScripts/SendAlarmToSlack.py
+++ UserScripts/SendAlarmToSlack.py
@@ -4,7 +4,9 @@
 import argparse
 import subprocess
 
-def SendAlarmToSlack(message, url="https://hooks.slack.com/services/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"):
+#
+
+def SendAlarmToSlack(message, url="https://hooks.slack.com/services/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"): 
         try:
                 # Construct the curl command to send the message to Slack
                 command = f"curl -X POST -H 'Content-type: application/json' --data '{{\"text\":\"{message}\"}}' {url}"
@@ -13,8 +15,8 @@ def SendAlarmToSlack(message, url="https://hooks.slack.com/services/T314VMYV8/B0
                 print("Message sent to Slack successfully")
         except subprocess.CalledProcessError as error:
                 print(f"Error sending message to Slack: {error}")
-
         return
+
 
 def main():
         # Create an ArgumentParser object
