3a7d7e7e39763ffe5220b6288a83bcf147f8ad59
RYAN A RIVERA <rrivera@fnal.gov>
2024-05-03 13:48:02 -0500

    Adding DTC program/reset tools for AL9

---
+++ tools/program_both_DTCs_AL9.sh
@@ -0,0 +1,43 @@
+source /home/xilinx/Vivado_Lab/2021.2/settings64.sh
+
+
+SCRIPT_DIR="$( 
+ cd "$(dirname "$(readlink "$0" || printf %s "$0")")"
+ pwd -P 
+)"
+HOSTNAME="$(hostname -f)"
+
+echo -e "program_both_DTCs.sh:${LINENO} |  \t Programming both bitfiles on ${HOSTNAME}..."
+echo -e "program_both_DTCs.sh:${LINENO} |  \t Number of arguments: $#"
+BITFILE0=$1
+BITFILE1=$1
+if [ $# == 1 ]; then
+    echo -e "program_both_DTCs.sh:${LINENO} |  \t Loading this bitfile to both DTCS: ${BITFILE0}" 
+elif [ $# == 2 ]; then
+    BITFILE1=$2
+else
+    echo -e "program_both_DTCs.sh:${LINENO} |  \t Illegal number of arguments, must be 1 or 2 to specify the bitfile for JTAG-0 and JTAG-1"
+    echo -e "\t usage 1 arg:  program_both_DTCs.sh <bitfile for both>"
+    echo -e "\t usage 2 args: program_both_DTCs.sh <bitfile for JTAG-0> <bitfile for JTAG-1>"
+    echo
+    return  >/dev/null 2>&1 #return is used if script is sourced
+	exit  #exit is used if script is run
+fi
+
+echo -e "program_both_DTCs.sh:${LINENO} |  \t JTAG-0 bitfile: ${BITFILE0}" 
+echo -e "program_both_DTCs.sh:${LINENO} |  \t JTAG-1 bitfile: ${BITFILE1}" 
+echo
+echo -e "program_both_DTCs.sh:${LINENO} |  \t vivado_lab -mode batch -source ${SCRIPT_DIR}/program_both_DTCs.tcl -tclargs ${BITFILE0} ${BITFILE1}" 
+vivado_lab -mode batch -source ${SCRIPT_DIR}/program_both_DTCs.tcl -tclargs ${BITFILE0} ${BITFILE1} 
+echo
+echo -e "program_both_DTCs.sh:${LINENO} |  \t Done programming bitfile to both DTCs on ${HOSTNAME}"
+
+
+#now reset
+echo -e "program_both_DTCs.sh:${LINENO} |  \t Resetting PCIe on ${HOSTNAME}..."
+source ${SCRIPT_DIR}/reset_PCIe_AL9.sh
+
+echo echo
+echo echo
+echo -e "program_both_DTCs.sh:${LINENO} |  \t ===> Done with ${HOSTNAME} programming bitfile and PCIe reset!"
+echo echo
---
+++ tools/reset_PCIe_AL9.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+#source /home/xilinx/Vivado_Lab/2021.2/settings64.sh
+
+
+SCRIPT_DIR="$( 
+ cd "$(dirname "$(readlink "$0" || printf %s "$0")")"
+ pwd -P 
+)"
+HOSTNAME="$(hostname -f)"
+
+echo "SCRIPT_DIR: ${SCRIPT_DIR}"
+
+RegEx='Xilinx.*704[23]'
+
+lspci | grep "$RegEx" && foundXi=1 || foundXi=0
+
+if [ "$foundXi" = 1 ];then
+    echo "Found DTC or CFO (Xilinx) card; removing mu2e driver;"
+    echo "first killing any processes that may be using the device."
+    pids=`lsof /dev/mu2e* 2>/dev/null | awk '!/^COMMAND/{print$2;}' | uniq`
+    test -n "$pids" && { echo "First attempt to kill $pids (which are using /dev/mu2e?)"; kill $pids; }
+    killall -9 xdaq.exe
+    sleep 3
+    rmmod mu2e
+    lsmod | grep mu2e && { echo "FAILURE - mu2e kernel module failed to unload!"; exit 1; }
+
+
+    echo
+    echo "Removing each PCIe Xilinx device on ${HOSTNAME}..."
+    echo
+    while read -r line
+    do
+        echo "$line"
+        IFS=' ' read -r -a array <<< "$line"
+
+        # for p in ${array[@]}; do
+        #     echo $p
+        # done
+        
+        echo "1" > /sys/bus/pci/devices/0000:${array[0]}/remove
+
+    done <<EOF
+    $(lspci | grep "$RegEx")
+EOF
+else
+    echo "No $RegEx cards found -- no need to unload mu2e kernel module"
+fi
+
+echo
+echo "Rescanning for PCIe devices on ${HOSTNAME}..."
+echo
+
+sleep 1
+echo "1" > /sys/bus/pci/rescan
+
+
+echo "Now attempt to reload mu2e module via modprobe mu2e"
+modprobe mu2e
+
+cd /root
+
+echo "Attempting to read firmware version on ${HOSTNAME}..."
+source ./setup_pcie_AL9.sh
+echo
+echo "PCIe Device 0 firmware version on ${HOSTNAME}:"
+my_cntl -d 0 read 0x9004 #device 0
+echo
+echo "PCIe Device 1 firmware version on ${HOSTNAME}:"
+my_cntl -d 1 read 0x9004 #device 1
+echo
+
+cd - >/dev/null 2>&1 
+echo
+echo "Done with ${HOSTNAME} PCIe reset script!"
+echo
+
