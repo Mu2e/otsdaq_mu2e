337bce04619ef5cbb46ceb57d7b023e8904cf30c
Eric Flumerfelt <eflumerf@fnal.gov>
2025-01-08 15:26:20 -0600

    Remove whitespace

--- tools/setup_vivado_lab_AL9.sh
+++ tools/setup_vivado_lab_AL9.sh
@@ -2,7 +2,7 @@
 
 source /home/xilinx/Vivado_Lab/2021.2/settings64.sh
 
-SCRIPT_DIR="$( 
+SCRIPT_DIR="$(
     cd "$(dirname "$(readlink "${BASH_SOURCE[0]}" || printf %s "${BASH_SOURCE[0]}")")" 
     pwd -P 
 )"
