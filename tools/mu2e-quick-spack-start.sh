0aefbb4a21d294d1c5d8dbd0625e53697db7c4d8
eflumerf <61473357+eflumerf@users.noreply.github.com>
2025-01-27 19:26:12 -0600

    Add --trigger option as synonym to --all-packages

--- tools/mu2e-quick-spack-start.sh
+++ tools/mu2e-quick-spack-start.sh
@@ -29,6 +29,8 @@ prompted for this location.
 --develop     Install the develop version of the software (may be unstable!)
 --tag         Install a specific tag of otsdaq
 --spackdir    Install Spack in this directory (or use existing installation)
+--all-packages Install all packages including Offline and otsdaq-mu2e-trigger
+--trigger     Synonym for --all-packages
 -a            Artdaq version number (e.g. 31300 for v3_13_00)
 -a            Otsdaq version number (e.g. 20800 for v2_08_00)
 -s            Use specific qualifiers when building ots
@@ -80,6 +82,7 @@ while [ -n "${1-}" ];do
             -arch)      eval $op1arg; arch=$1; shift;;
             -no-kmod)   opt_no_kmod=1;;
             -all-packages) opt_all_packages=1;;
+	    -trigger)   opt_all_packages=1;;
             -no-view)   opt_no_view=1;;
             *)          echo "Unknown option -$op"; do_help=1;;
         esac
