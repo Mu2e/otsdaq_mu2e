a999954ad0328b1e03006c039bcb4ed0803cae71
eflumerf <61473357+eflumerf@users.noreply.github.com>
2025-02-25 09:53:34 -0600

    Fix otsdaq-mu2e URL

--- tools/mu2e-quick-spack-start.sh
+++ tools/mu2e-quick-spack-start.sh
@@ -119,7 +119,7 @@ if [ -z "${tag:-}" ]; then
 fi
 
 rm CMakeLists.txt*
-wget https://raw.githubusercontent.com/Mu2e/otsdaq_mu2e/$tag/CMakeLists.txt
+wget https://raw.githubusercontent.com/Mu2e/otsdaq-mu2e/$tag/CMakeLists.txt
 demo_version=v`grep "project" $Base/CMakeLists.txt|grep -oE "VERSION [^)]*"|awk '{print $2}'|sed 's/\./_/g'`
 echo "Mu2e TDAQ Version is $demo_version"
 if [[ $notag -eq 1 ]] && [[ $opt_develop -eq 0 ]]; then
@@ -127,7 +127,7 @@ if [[ $notag -eq 1 ]] && [[ $opt_develop -eq 0 ]]; then
 
   # 06-Mar-2017, KAB: re-fetch the product_deps file based on the tag
   mv CMakeLists.txt CMakeLists.txt.orig
-  wget https://raw.githubusercontent.com/Mu2e/otsdaq_mu2e/$tag/CMakeLists.txt
+  wget https://raw.githubusercontent.com/Mu2e/otsdaq-mu2e/$tag/CMakeLists.txt
   demo_version=v`grep "project" $Base/CMakeLists.txt|grep -oE "VERSION [^)]*"|awk '{print $2}'|sed 's/\./_/g'`
   tag=$demo_version
 fi
