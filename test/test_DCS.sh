abb885d54a24cb19441ba5ddc803da44fa43bfb3
RYAN A RIVERA <rrivera@FNAL.GOV>
2025-03-04 09:45:15 -0600

    Whitespace fix

--- test/test_DCS.sh
+++ test/test_DCS.sh
@@ -4,4 +4,3 @@
 # use grep to show only single lines from DCS_test source
 # use sed to hide TRACE preamble decoration
 DCS_test "$@" 2>&1 | grep "DCS_test.cc:" | grep '^|' | sed -E 's/\|.*\|(.*)/\1/'
-
