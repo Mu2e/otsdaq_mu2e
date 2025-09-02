5a9288e051614dd5559c6e277dd0c942bf44416f
Eric Flumerfelt <eflumerf@fnal.gov>
2025-09-02 08:54:50 -0500

    Always remove upstreams.yaml to make sure version updates don't confuse things

--- tools/mu2e-quick-spack-start.sh
+++ tools/mu2e-quick-spack-start.sh
@@ -245,6 +245,7 @@ if [ $opt_no_auto_upstream -eq 0 ] && [ -d /mu2e/spack_areas ];then
   upstreams+=($mu2e $ots $artdaq $art)
 fi
 
+rm $spackdir/etc/spack/upstreams.yaml # Remove as we will re-generate
 for upstream in ${upstreams[@]}; do
     for upstreamdir in `find $upstream -type f -wholename '*/.spack-db/index.json' 2>/dev/null`; do
         echo "Getting real directory for upstream database $upstreamdir"
