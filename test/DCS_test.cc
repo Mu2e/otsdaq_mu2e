9192ab42f0ad50678db5735d3bb5c9bbeb5b3cfc
RYAN RIVERA <rrivera@FNAL.GOV>
2026-01-12 14:15:15 -0600

    Cleaned up use of env var utility, moved utility to otsdaq repo

--- test/DCS_test.cc
+++ test/DCS_test.cc
@@ -18,6 +18,9 @@
 // #include "otsdaq-components/FEInterfaces/FEWOtsUDPFSSRInterface.h"
 #include "otsdaq-mu2e/FEInterfaces/DTCFrontEndInterface.h"
 
+// Shared test utilities
+#include "otsdaq/Macros/TestUtilities.h"
+
 using namespace ots;
 
 int main(int argc, char* argv[])
@@ -110,44 +113,12 @@ try
 		return 0;
 	}
 
+
 	//==============================================================================
 	// Define environment variables
 	//	Note: normally these environment variables are set by ots script
 
-	// These are needed by
-	// otsdaq/otsdaq/ConfigurationDataFormats/ConfigurationInfoReader.cc [207]
-	setenv("CONFIGURATION_TYPE", "File", 1);  // Can be File, Database, DatabaseTest
-	setenv("CONFIGURATION_DATA_PATH",
-	       (std::string(getenv("USER_DATA")) + "/ConfigurationDataExamples").c_str(),
-	       1);
-	setenv(
-	    "TABLE_INFO_PATH", (std::string(getenv("USER_DATA")) + "/TableInfo").c_str(), 1);
-	////////////////////////////////////////////////////
-
-	// Some configuration plug-ins use __ENV__("OTSDAQ_LIB") and
-	// __ENV__("OTSDAQ_UTILITIES_LIB") in init() so define it 	to a non-sense place is ok
-	setenv("OTSDAQ_LIB", (std::string(getenv("USER_DATA")) + "/").c_str(), 1);
-	setenv("OTSDAQ_UTILITIES_LIB", (std::string(getenv("USER_DATA")) + "/").c_str(), 1);
-
-	// Some configuration plug-ins use __ENV__("OTS_MAIN_PORT") in init() so define it
-	setenv("OTS_MAIN_PORT", "2015", 1);
-
-	// also xdaq envs for XDAQContextTable
-	setenv("XDAQ_CONFIGURATION_DATA_PATH",
-	       (std::string(getenv("USER_DATA")) + "/XDAQConfigurations").c_str(),
-	       1);
-	setenv("XDAQ_CONFIGURATION_XML", "otsConfigurationNoRU_CMake", 1);
-
-	if(getenv("OTSDAQ_LOG_FHICL") == NULL)
-		setenv("OTSDAQ_LOG_FHICL",
-		       (std::string(__ENV__("USER_DATA")) +
-		        "/MessageFacilityConfigurations/MessageFacilityWithCout.fcl")
-		           .c_str(),
-		       1);
-
-	if(getenv("OTSDAQ_LOG_ROOT") == NULL)
-		setenv(
-		    "OTSDAQ_LOG_ROOT", (std::string(__ENV__("USER_DATA")) + "/Logs").c_str(), 1);
+	test::util::check_and_make_envs();
 
 	////////////////////////////////////////////////////
 
