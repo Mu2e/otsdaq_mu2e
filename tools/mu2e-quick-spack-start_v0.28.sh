71913a2b3aa08f33f5d02d431e9512b365284274
Mu2e <mu2eshift@mu2e-dl-01.fnal.gov>
2026-02-18 15:19:51 -0600

    quick-spack-start: adding option to also check out otsdaq and artdaq for dev work

--- tools/mu2e-quick-spack-start_v0.28.sh
+++ tools/mu2e-quick-spack-start_v0.28.sh
@@ -47,6 +47,8 @@ prompted for this location.
 --no-use-cvmfs Do not search /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_v0.28 for upstreams
               Note that CVMFS will not be used if /mu2e is available and active (i.e. --no-use-mu2e is not passed)
               If --upstream is used, neither /mu2e nor CVMFS will be automatically searched for upstreams
+--dev-otsdaq  Checks out otsdaq, otsdaq-utilities, and otsdaq-components
+--dev-artdaq  Checks out artdaq, artdaq-core, artdaq-daqinterface, and artdaq-database
 --all-packages Used with --develop, will fetch all subdetector repos
 --g4          Perform full build of Offline, with geant4 dependency
 "
@@ -91,7 +93,9 @@ while [ -n "${1-}" ];do
             -no-emacs)  opt_no_emacs=1;;
             -no-use-mu2e) opt_use_mu2e=0;;
             -no-use-cvmfs) opt_use_cvmfs=0;;
-            -all-packages) opt_all_packages=1;;
+            -dev-otsdaq) opt_dev_otsdaq=1;;\
+        -dev-artdaq) opt_dev_artdaq=1;;\
+        -all-packages) opt_all_packages=1;;
             -trigger)   opt_all_packages=1;;
             -no-view)   opt_no_view=1;;
 	        -g4)        opt_g4=1;;
@@ -113,6 +117,50 @@ fi
 
 # Save all output from this script (stdout + stderr) in a file with a
 # name that looks like "quick-start.sh_Fri_Jan_16_13:58:27.script" as
+
+if [ "${opt_dev_otsdaq-0}" -eq 1 ]; then
+    echo "Checking out otsdaq, otsdaq-utilities, and otsdaq-components..."
+    if ! [ -d srcs ]; then
+        mkdir srcs
+    fi
+    cd srcs
+    for pkg in otsdaq otsdaq-utilities otsdaq-components; do
+        if ! [ -d $pkg ]; then
+            if [ $opt_w -eq 0 ]; then
+                git clone https://github.com/art-daq/$pkg.git $pkg
+            else
+                git clone git@github.com:art-daq/$pkg.git $pkg
+            fi
+        else
+            cd $pkg
+            git pull
+            cd ..
+        fi
+    done
+    cd ..
+fi
+
+if [ "${opt_dev_artdaq-0}" -eq 1 ]; then
+    echo "Checking out artdaq and artdaq-core..."
+    if ! [ -d srcs ]; then
+        mkdir srcs
+    fi
+    cd srcs
+    for pkg in artdaq artdaq-core artdaq-daqinterface artdaq-database; do
+        if ! [ -d $pkg ]; then
+            if [ $opt_w -eq 0 ]; then
+                git clone https://github.com/art-daq/$pkg.git $pkg
+            else
+                git clone git@github.com:art-daq/$pkg.git $pkg
+            fi
+        else
+            cd $pkg
+            git pull
+            cd ..
+        fi
+    done
+    cd ..
+fi
 # well as all stderr in a file with a name that looks like
 # "quick-start.sh_Fri_Jan_16_13:58:27_stderr.script"
 alloutput_file=$( date | awk -v "SCRIPTNAME=$(basename $0)" '{print SCRIPTNAME"_"$1"_"$2"_"$3"_"$4".script"}' )
