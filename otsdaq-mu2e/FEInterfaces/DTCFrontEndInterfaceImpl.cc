6d74128f610100b1882d726da4dfe4e4ccb3ff06
RYAN RIVERA <rrivera@FNAL.GOV>
2026-03-03 07:04:54 -0600

    Format fix

--- otsdaq-mu2e/FEInterfaces/DTCFrontEndInterfaceImpl.cc
+++ otsdaq-mu2e/FEInterfaces/DTCFrontEndInterfaceImpl.cc
@@ -1665,7 +1665,8 @@ void DTCFrontEndInterface::configureCommon(void)
 				if(getCFOandDTCRegisters()->isCRVDTCDesignFlavour())
 				{
 					__FE_COUT__ << "enable punched clock on CRV DTC" << __E__;
-					clockMakersEnabled = false;  // clock markers are always off for the CRV
+					clockMakersEnabled =
+					    false;  // clock markers are always off for the CRV
 				}
 				rocSetupString = SetupROCs(
 				    DTCLib::DTC_Link_ID(i),  //DTCLib::DTC_Link_ID rocLinkIndex,
