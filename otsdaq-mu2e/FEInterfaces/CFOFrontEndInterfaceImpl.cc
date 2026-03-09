06cdc5248e80e1d42d215705c1bd03fb275ee214
RYAN RIVERA <rrivera@FNAL.GOV>
2026-03-09 11:24:32 -0500

    Format fix

--- otsdaq-mu2e/FEInterfaces/CFOFrontEndInterfaceImpl.cc
+++ otsdaq-mu2e/FEInterfaces/CFOFrontEndInterfaceImpl.cc
@@ -3453,8 +3453,10 @@ void CFOFrontEndInterface::SharedRunPlanStart(__ARGS__)
 	//	Set Run Plan checks BRAM size indirectly, by reading back and validating the instruction set written!
 	{
 		//Start overwrites whatever is in hardware; use empty/default masks for both parts.
-		std::vector<uint64_t> emptyAndMasks(standardNValues_[0], 0xFFFFFFFFFFFFULL);  //keep-all
-		std::vector<uint64_t> emptyOrMasks( (standardNValues_.size() - 1) + standardNValues_[0], 0x0ULL);              //set-nothing
+		std::vector<uint64_t> emptyAndMasks(standardNValues_[0],
+		                                    0xFFFFFFFFFFFFULL);  //keep-all
+		std::vector<uint64_t> emptyOrMasks(
+		    (standardNValues_.size() - 1) + standardNValues_[0], 0x0ULL);  //set-nothing
 
 		//part-1: write the initial all-on mode plan
 		generateSharedRunPlanWithPeriodicModeOn(result,
