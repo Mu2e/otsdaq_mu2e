c14e708904738523a72b49ff91ab669f8bf78232
Eric Flumerfelt <eflumerf@fnal.gov>
2026-03-18 16:08:02 -0500

    Limit find to srcs depth

--- tools/mu2e-quick-spack-start_v0.28.sh
+++ tools/mu2e-quick-spack-start_v0.28.sh
@@ -150,7 +150,7 @@ if [ $opt_no_view -eq 1 ];then
     view_opt="--without-view"
 fi
 
-build_system_script=`find $Base -type f -name setup_spack_build_system_v0.28.sh`
+build_system_script=`find $Base -maxdepth 4 -type f -name setup_spack_build_system_v0.28.sh`
 if [[ "x$build_system_script" == "x" ]];then
   echo "WARNING: setup_spack_build_system_v0.28.sh not found, downloading from https://github.com/art-daq/artdaq-demo"
   cd $Base
