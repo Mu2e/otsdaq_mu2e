fda46969baacd96dc789e0465a1dd2292b8e2410
RYAN RIVERA <rrivera@FNAL.GOV>
2026-05-14 11:50:58 -0500

    Format fix

--- otsdaq-mu2e/FEInterfaces/DTCFrontEndInterfaceImpl.cc
+++ otsdaq-mu2e/FEInterfaces/DTCFrontEndInterfaceImpl.cc
@@ -5961,10 +5961,11 @@ try
 			    << " iteration=" << ii
 			    << " subeventsCount=" << threadStruct->subeventsCount_;
 
-			while((subevents = threadStruct->thisDTC_->GetSubEventData2( //GetSubEventData
-			           DTCLib::DTC_EventWindowTag(threadStruct->nextEventWindowTag_),
-			           false /* EWT match */))
-			          .size())
+			while(
+			    (subevents = threadStruct->thisDTC_->GetSubEventData2(  //GetSubEventData
+			         DTCLib::DTC_EventWindowTag(threadStruct->nextEventWindowTag_),
+			         false /* EWT match */))
+			        .size())
 			{
 				if(threadStruct->exitThread_)
 				{
@@ -5995,14 +5996,16 @@ try
 
 					// Print first and last 8 qwords of the returned subevent to confirm data is copied correctly
 					{
-						const uint8_t* raw      = reinterpret_cast<const uint8_t*>(subeventPtr->GetRawBufferPointer());
-						const size_t   rawBytes = subeventPtr->GetSubEventByteCount();
+						const uint8_t* raw = reinterpret_cast<const uint8_t*>(
+						    subeventPtr->GetRawBufferPointer());
+						const size_t      rawBytes = subeventPtr->GetSubEventByteCount();
 						std::stringstream fss, lss;
 						fss << "GetSubEventData2 returned subevent rawBytes=" << rawBytes
 						    << " first 8 qwords: ";
 						for(int _i = 0; _i < 8 && (size_t)(_i * 8) < rawBytes; ++_i)
 							fss << std::hex << std::setw(16) << std::setfill('0')
-							    << *reinterpret_cast<const uint64_t*>(raw + _i * 8) << " ";
+							    << *reinterpret_cast<const uint64_t*>(raw + _i * 8)
+							    << " ";
 						__GEN_COUTT__ << fss.str() << __E__;
 
 						lss << "GetSubEventData2 returned subevent last 8 qwords: ";
